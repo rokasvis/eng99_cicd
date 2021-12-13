30b5ed20b9c3d102ed2d2bd30ffc923fc6dcd0b6
Rokas <rvisockis@spartaglobal.com>
2021-12-13 21:07:35 +0000

    updated provisioning file

--- provision.sh
+++ provision.sh
@@ -6,6 +6,7 @@ sudo apt-get install nginx -y
 suod systemctl enable nginx
 sudo apt-get install git -y
 sudo apt-get install python-software-properties
+sudo apt-get purge nodejs -y
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install nodejs -y
 sudo npm install pm2 -g
