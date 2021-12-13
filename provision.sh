924677550c61620a687a7f9debd28e1004e3db41
Rokas <rvisockis@spartaglobal.com>
2021-12-13 20:42:21 +0000

    updated provisioning file

--- provision.sh
+++ provision.sh
@@ -8,3 +8,4 @@ sudo apt-get install git -y
 sudo apt-get install python-software-properties
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install nodejs -y
+sudo npm install pm2 -g
